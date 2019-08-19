982eb689bba1d498e456ebff340fc5906f8435a4
Vasif Mustafayev <vmustafayev@gmail.com>
2019-08-19 21:31:19 +0200

    Added right turn sign logic as well

--- turn-signal-for-bike.ino
+++ turn-signal-for-bike.ino
@@ -10,15 +10,15 @@ const int arrowMarqueStepsCount = 16;
 bool isTurningLeft = false;
 bool isTurningRight = false;
 
-const int lefTurnButtonPin = 4;
+const int leftTurnButtonPin = 4;
 const int rightTurnButtonPin = 5;
 
 const int buzzerPin = 3;
 
 // (DATA IN, CLK, LOAD(CS), count of attached MAX72XX)
 const int dataInPin = 12;
-const int clkPin = 11;
-const int csPin = 10;
+const int csPin = 11;
+const int clkPin = 10;
 const int max72XXCount = 1;
 LedControl ledCtrl = LedControl(dataInPin, clkPin, csPin, max72XXCount);
 byte leftArrowAnimation[arrowMarqueStepsCount][8] = {
@@ -39,6 +39,24 @@ byte leftArrowAnimation[arrowMarqueStepsCount][8] = {
   {B00000000, B00000000, B00000000, B10000000, B10000000, B00000000, B00000000, B00000000},
   {B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000},
 };
+byte rightArrowAnimation[arrowMarqueStepsCount][8] = {
+  {B00000000, B00000000, B00000000, B10000000, B10000000, B00000000, B00000000, B00000000},
+  {B00000000, B00000000, B10000000, B11000000, B11000000, B10000000, B00000000, B00000000},
+  {B00000000, B10000000, B11000000, B11100000, B11100000, B11000000, B10000000, B00000000},
+  {B10000000, B11000000, B11100000, B11110000, B11110000, B11100000, B11000000, B10000000},
+  {B01000000, B01100000, B01110000, B11111000, B11111000, B01110000, B01100000, B01000000},
+  {B00100000, B00110000, B00111000, B11111100, B11111100, B00111000, B00110000, B00100000},
+  {B00010000, B00011000, B00011100, B11111110, B11111110, B00011100, B00011000, B00010000},
+  {B00001000, B00001100, B00001110, B11111111, B11111111, B00001110, B00001100, B00001000},
+  {B00000100, B00000110, B00000111, B01111111, B01111111, B00000111, B00000110, B00000100},
+  {B00000010, B00000011, B00000011, B00111111, B00111111, B00000011, B00000011, B00000010},
+  {B00000001, B00000001, B00000001, B00011111, B00011111, B00000001, B00000001, B00000001},
+  {B00000000, B00000000, B00000000, B00001111, B00001111, B00000000, B00000000, B00000000},
+  {B00000000, B00000000, B00000000, B00000111, B00000111, B00000000, B00000000, B00000000},
+  {B00000000, B00000000, B00000000, B00000011, B00000011, B00000000, B00000000, B00000000},
+  {B00000000, B00000000, B00000000, B00000001, B00000001, B00000000, B00000000, B00000000},
+  {B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000}
+};
 
 void setup() {
   //Begin Serial communication at a baudrate of 9600 - for testing purpose:
@@ -49,26 +67,40 @@ void setup() {
   ledCtrl.setIntensity(0, 1); //medium brightness
   clearDisplayFor(ledCtrl);
 
-  pinMode(lefTurnButtonPin, INPUT_PULLUP);
+  pinMode(leftTurnButtonPin, INPUT_PULLUP);
+  pinMode(rightTurnButtonPin, INPUT_PULLUP);
 }
 
 void loop() {
   if (isTurningLeft)
     displaySymbol(leftArrowAnimation);
   else if (isTurningRight)
-    displaySymbol(leftArrowAnimation);//TODO: right animation goes here
+    displaySymbol(rightArrowAnimation);
   else
     clearDisplayFor(ledCtrl);
 
   // button precessed
-  if (digitalRead(lefTurnButtonPin) == HIGH) {
-    Serial.println("+++++++++++++++++++++++++++++++++++++++++");
+  if (digitalRead(leftTurnButtonPin) == HIGH) {
+    Serial.println("+++++++++++++++++++++++++++++++++++++++++left");
     if (isTurningLeft) {
       clearDisplayFor(ledCtrl);
       isTurningLeft = false;
       delay(250);
     } else {
+      isTurningRight = false;
       isTurningLeft = true;
+      delay(150);
+    }
+  }
+  else if (digitalRead(rightTurnButtonPin) == HIGH) {
+    Serial.println("+++++++++++++++++++++++++++++++++++++++++right");
+    if (isTurningRight) {
+      clearDisplayFor(ledCtrl);
+      isTurningRight = false;
+      delay(250);
+    } else {
+      isTurningLeft = false;
+      isTurningRight = true;
       delay(150);
     }
   }
@@ -87,17 +119,17 @@ void displaySymbol(byte symbols[arrowMarqueStepsCount][8]) {
     }
 
     delay(30);
-    if (s%9 == 0)
+    if (s % 9 == 0)
       tone(buzzerPin, 50);
     delay(20);
     noTone(buzzerPin);
-    
+
     // Clearing dots
     for (int i = 0; i < 8; i++) {
       ledCtrl.setRow(0, i, 0);
     }
 
-    if (digitalRead(lefTurnButtonPin) == HIGH || digitalRead(rightTurnButtonPin) == HIGH) {
+    if (digitalRead(leftTurnButtonPin) == HIGH || digitalRead(rightTurnButtonPin) == HIGH) {
       delay(1);
       break;
     }
@@ -110,10 +142,10 @@ void clearDisplayFor(LedControl ledControl) {
   ledControl.clearDisplay(0);
 }
 
-void turnSignalVoice(){
-    tone(buzzerPin, 50);
-    delay(20);
-    noTone(buzzerPin);
-    delay(500);
-    tone(buzzerPin, 50);
+void turnSignalVoice() {
+  tone(buzzerPin, 50);
+  delay(20);
+  noTone(buzzerPin);
+  delay(500);
+  tone(buzzerPin, 50);
 }
